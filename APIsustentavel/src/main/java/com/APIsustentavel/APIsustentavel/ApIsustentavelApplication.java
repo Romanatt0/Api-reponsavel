5f546e7360d7571d698c9e085f2d25e5cd665654
Victor.Romanatto <victorcesarromanatto@gmail.com>
2025-09-02 18:02:55 -0300

    Git init

---
+++ APIsustentavel/src/main/java/com/APIsustentavel/APIsustentavel/ApIsustentavelApplication.java
@@ -0,0 +1,13 @@
+package com.APIsustentavel.APIsustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIsustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIsustentavelApplication.class, args);
+	}
+
+}
---
+++ APIsustentavel/src/test/java/com/APIsustentavel/APIsustentavel/ApIsustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIsustentavel.APIsustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIsustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
